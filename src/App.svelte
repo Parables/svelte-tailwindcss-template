6db9aa8f75cb603fad61086c1faadeee71ed15d0
Parables Boltnoel <parables95@gmail.com>
2020-02-07 17:23:24 +0000

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+
+</script>
+
+<div class="max-w-sm mx-auto flex p-6 bg-white rounded-lg p-12 shadow-xl">
+  <div class="flex-shrink-0">
+    <img class="h-12 w-12" src="" alt="ChitChat Logo" />
+  </div>
+  <div class="ml-6 pt-1">
+    <h4 class="text-xl text-green-600 leading-tight">ChitChat</h4>
+    <p class="text-base text-purple-500 leading-normal">
+      You have a new message
+    </p>
+  </div>
+</div>
